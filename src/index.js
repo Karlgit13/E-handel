5be36259ae9428a690482ca3c9a9d945bde91585
Karlgit13 <karl.vareskog@gmail.com>
2024-02-05 19:19:01 +0100

    Add banners and update routes in App.js and ShopCategory.jsx

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ShopContextProvider from './Context/ShopContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ShopContextProvider>
     <App />
-  </React.StrictMode>
+  </ShopContextProvider>
 );
 
