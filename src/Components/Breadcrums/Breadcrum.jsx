dcde2c786a6da62eafe481fc54af02459cef6f44
Karlgit13 <karl.vareskog@gmail.com>
2024-02-05 19:51:03 +0100

    Add link to product page and breadcrum component

---
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Breadcrum.css";
+import arrow_icon from "../../Assets/arrow.png";
+
+const Breadcrum = (props) => {
+  const { product } = props;
+
+  return (
+    <div className="breadcrum">
+      HOME <img src={arrow_icon} alt="arrowIcon" /> SHOP{" "}
+      <img src={arrow_icon} alt="arrowIcon" /> {product.category}{" "}
+      <img src={arrow_icon} alt="" /> {product.name}
+    </div>
+  );
+};
+
+export default Breadcrum;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  return (
+    <div>
+      <Breadcrum product={product} />
+    </div>
+  );
 };
 
 export default Product;
