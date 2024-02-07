bf14a7a8c573c444c8119b45dd84dcdce5f0645a
Karlgit13 <karl.vareskog@gmail.com>
2024-02-07 12:01:34 +0100

    Add onClick event to item image and implement addToCart function

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import CartItems from "../Components/CartItems/CartItems";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return <div>
+    <CartItems />
+  </div>;
 };
 
 export default Cart;
