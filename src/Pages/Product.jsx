4684ca36dd3ef747d43890299c09e7ab27ade60b
Karlgit13 <karl.vareskog@gmail.com>
2024-02-07 07:30:39 +0100

    Add Breadcrum styling and display product details

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -11,6 +13,8 @@ const Product = () => {
   return (
     <div>
       <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+      <DescriptionBox />
     </div>
   );
 };
