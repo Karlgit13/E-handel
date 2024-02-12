9265359838e983428815baa521f3992c32762318
Karlgit13 <karl.vareskog@gmail.com>
2024-02-12 13:21:03 +0100

    Update CSS styles for grid layout and responsiveness

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -46,7 +46,7 @@ const ShopCategory = (props) => {
 
   return (
     <div className="shop-category">
-      <img src={props.banner} alt="banner" />
+      <img className="shop-category-banner" src={props.banner} alt="banner" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
